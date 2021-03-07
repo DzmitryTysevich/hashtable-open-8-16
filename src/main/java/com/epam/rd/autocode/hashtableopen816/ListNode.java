e0b5ba7985953a01c3688701714578a56c5b3114
DzmitryTysevich <dztysevich@gmail.com>
2021-03-07 15:59:17 +0300

    Initial commit

---
+++ src/main/java/com/epam/rd/autocode/hashtableopen816/ListNode.java
@@ -0,0 +1,36 @@
+package com.epam.rd.autocode.hashtableopen816;
+
+public class ListNode {
+    private Integer key;
+    private Object value;
+    private boolean isRemoved;
+
+    public void remove() {
+        isRemoved = true;
+    }
+
+    public boolean notRemoved() {
+        return !isRemoved;
+    }
+
+    public Integer getKey() {
+        return key;
+    }
+
+    public void setKey(Integer key) {
+        this.key = key;
+    }
+
+    public Object getValue() {
+        return value;
+    }
+
+    public void setValue(Object value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return key.toString();
+    }
+}
